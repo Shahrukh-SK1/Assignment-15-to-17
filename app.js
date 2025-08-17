336d653fcf6f1ee7eebb72ae418131f22159120e
Shahrukh-SK1 <sharukhayub6111@gmail.com>
2025-08-17 21:34:17 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+// Answer No. 1 (Array Literal Notation)
+
+var studentNames = [];
+
+// Answer No. 2 (Array Object Notation)
+
+var studentNames = new Array();
+
+// Answer No. 3 (String Array)
+
+var stringArray = ["Shahrukh", "Rayyan", "Ali", "Danish", "Azeem"];
+
+
+// Answer No. 4 (Numbers Array)
+
+var numbersArray = [10, 20, 30, 40, 50];
+
+// Answer No. 5 (Boolean Array)
+
+var booleanArray = [true, false];
+
+
+// Answer No. 6 (Mix Array)
+
+var mixArray = ["Shahrukh", 1, true, "Rayyan", true, 2,];
